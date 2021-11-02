0084024b82f23782dc38334714289d0102a72808
Dschahannam <uwunoozles@gmail.com>
2021-11-02 11:04:16 +0100

    Cleanup & Comment

--- src/anticheat/detections.cpp
+++ src/anticheat/detections.cpp
@@ -1,22 +1,33 @@
 #include <anticheat\anticheat.hpp>
 #include <anticheat\detections.hpp>
 
+/**
+ * Detection-Rate: 100%
+ * False-Flag Rate: 0-0.5%
+ * 
+ * Before any cheat creates their thread, they call 'DisableThreadLibraryCalls'.
+ * Currently only tested during runtime. Needs to be tested at game start to see how it behaves.
+ */
 BOOL WINAPI hook_disable_thread_library_calls(_In_ HMODULE lib_module) {
 	anticheat_detections::get().detect_by_type(anticheat_detections::_DetectionTypes::DETECTION_DISABLE_THREAD_LIBRARY_CALLS);
 
 	return anticheat_detections::get().o_disable_thread_library_calls(lib_module);
 }
 
+
+/**
+ * Detection-Rate: 50-60%
+ * False-Flag Rate: 0-5%
+ * 
+ * Some cheats create a file to pass data from loader to client.
+ * NVIDIA & ReShade may also this.
+ */
 HANDLE WINAPI hook_create_file(_In_ LPCWSTR lpFileName, _In_ DWORD dwDesiredAccess, _In_ DWORD dwShareMode, _In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes, _In_ DWORD dwCreationDisposition, _In_ DWORD dwFlagsAndAttributes, _In_opt_ HANDLE hTemplateFile) {
 	wstring w_string(lpFileName);
 	string file_name = string(w_string.begin(), w_string.end());
 
 	if (file_name.find(".ini") != std::string::npos) {
 		if (file_name.find("ReShade") == std::string::npos) {
-			// Bei 0xCheats wird beim Injecten CreateFile gecalled.
-			// In dieser INI Datei liegen dann Daten über dem Spieler.
-			// Nur Gott weiß warum gleichzeitig dann auch dasselbe bei ReShade gecalled wird.
-
 			anticheat_detections::get().detect_by_type(anticheat_detections::_DetectionTypes::DETECTION_CREATE_FILE);
 		}
 	}
@@ -24,6 +35,13 @@ HANDLE WINAPI hook_create_file(_In_ LPCWSTR lpFileName, _In_ DWORD dwDesiredAcce
 	return anticheat_detections::get().o_create_file(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
 }
 
+/**
+ * Detection-Rate: 95%
+ * False-Flag Rate: 0%
+ * 
+ * Detects loading & writing any types of config or authentification files.
+ * Usually theres no module who uses std::ifstream.
+ */
 FILE* __cdecl hook_fs_open(_In_z_ char const* _FileName, _In_z_ char const* _Mode, _In_ int _ShFlag) {
 	string file_name = _FileName;
 
@@ -44,6 +62,10 @@ void anticheat_detections::run_service()
 
 	MH_CreateHook(&_fsopen, &hook_fs_open, reinterpret_cast<LPVOID*>(&o_fs_open));
 	MH_EnableHook(&_fsopen);
+
+	// TODO: add virtual_protect hook to detect detour hookings
+	// ULONG DETOUR_REGION_SIZE = 0x10000;
+	// PAGE_EXECUTE_READWRITE
 }
 
 void anticheat_detections::detect_by_type(_DetectionTypes detection_type, const char* detection_info = "")
@@ -67,7 +89,7 @@ const char* anticheat_detections::detection_to_string(_DetectionTypes detection_
 			ST2STR(DETECTION_FS_OPEN)
 	}
 
-#undef MH_ST2STR
+#undef ST2STR
 
 	return "(unknown)";
 }
