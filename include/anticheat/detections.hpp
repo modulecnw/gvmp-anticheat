ac95ff4bb501cc1111adae31b8a9d3eaf7bd693a
Dschahannam <uwunoozles@gmail.com>
2021-11-02 19:14:12 +0100

    Add LoadLibrary Detection

--- include/anticheat/detections.hpp
+++ include/anticheat/detections.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "imports.hpp"
+#include <SubAuth.h>
+#include <TlHelp32.h>
 
 #define ANTICHEAT
 using namespace std;
@@ -7,16 +9,23 @@ using namespace std;
 class anticheat_detections : public c_singleton<anticheat_detections>
 {
 private:
+	typedef NTSTATUS(NTAPI* pldr_manifest_prober_routine_t)(IN HMODULE dll_base, IN PCWSTR full_dll_path, OUT PHANDLE activation_context);
+	typedef NTSTATUS(NTAPI* pldr_actx_language_rourine_t)(IN HANDLE unk, IN USHORT lang_id, OUT PHANDLE activation_context);
+	typedef void(NTAPI* pldr_release_act_routine_t)(IN HANDLE activation_context);
+	typedef VOID(NTAPI* ldr_set_dll_manifest_prober_t)(IN pldr_manifest_prober_routine_t ManifestProberRoutine, IN pldr_actx_language_rourine_t CreateActCtxLanguageRoutine, IN pldr_release_act_routine_t ReleaseActCtxRoutine);
+
 	typedef LONG(WINAPI* nt_protect_virtual_memory_t)(IN HANDLE process_handle, IN OUT PVOID* base_address, IN OUT PULONG number_of_bytes_to_protect, IN ULONG new_access_protection, OUT PULONG old_access_protection);
 	typedef BOOL(WINAPI* disable_thread_library_calls_t)(_In_ HMODULE lib_module);
 	typedef HANDLE(WINAPI* create_file_t)(_In_ LPCWSTR lpFileName, _In_ DWORD dwDesiredAccess, _In_ DWORD dwShareMode, _In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes, _In_ DWORD dwCreationDisposition, _In_ DWORD dwFlagsAndAttributes, _In_opt_ HANDLE hTemplateFile);
 	typedef FILE* (__cdecl* fs_open_t)(_In_z_ char const* _FileName, _In_z_ char const* _Mode, _In_ int _ShFlag);
+
 public:
 	typedef enum _DetectionTypes {
 		DETECTION_UNKNOWN = 1,
 		DETECTION_DISABLE_THREAD_LIBRARY_CALLS,
 		DETECTION_CREATE_FILE,
-		DETECTION_FS_OPEN
+		DETECTION_FS_OPEN,
+		DETECTION_DLL_MANIFEST_PROBER_CALLBACK
 	};
 
 	nt_protect_virtual_memory_t o_nt_protect_virtual_memory = nullptr;
--- src/anticheat/detections.cpp
+++ src/anticheat/detections.cpp
@@ -51,12 +51,51 @@ HANDLE WINAPI hook_create_file(_In_ LPCWSTR lpFileName, _In_ DWORD dwDesiredAcce
 FILE* __cdecl hook_fs_open(_In_z_ char const* _FileName, _In_z_ char const* _Mode, _In_ int _ShFlag) {
 	string file_name = _FileName;
 
-	Log::Debug("FS", file_name);
 	if (file_name.find(".ini") != std::string::npos || file_name.find(".token") != std::string::npos || file_name.find(".cfg") != std::string::npos) {
 		anticheat_detections::get().detect_by_type(anticheat_detections::_DetectionTypes::DETECTION_FS_OPEN);
 	}
 
 	return anticheat_detections::get().o_fs_open(_FileName, _Mode, _ShFlag);
+}
+
+/**
+ * Detection-Rate: 100%
+ * False-Flag Rate: 0%
+ *
+ * Detects standard LoadLibrary injections.
+ * Example: GH-Injector, Xenos-64, etc.
+ */
+NTSTATUS NTAPI main_dll_manifest_prober_callback(IN HMODULE dll_base, IN PCWSTR full_dll_path, OUT PHANDLE activation_context) {
+	DWORD64 current_tick = GetTickCount64();
+
+	static DWORD64 current_tick_prober_callback = 0x0;
+	static DWORD64 latest_tick_prober_callback = 0x0;
+
+	if ((current_tick_prober_callback - latest_tick_prober_callback) > 4 * 1000) {
+		anticheat_detections::get().detect_by_type(anticheat_detections::_DetectionTypes::DETECTION_DLL_MANIFEST_PROBER_CALLBACK);
+
+		latest_tick_prober_callback = current_tick_prober_callback;
+	}
+	current_tick_prober_callback = current_tick;
+
+	if (!*activation_context) return STATUS_INVALID_PARAMETER;
+
+	HANDLE actx = NULL;
+	ACTCTXW act = { 0 };
+
+	act.cbSize = sizeof(act);
+	act.dwFlags = ACTCTX_FLAG_RESOURCE_NAME_VALID | ACTCTX_FLAG_HMODULE_VALID;
+	act.lpSource = full_dll_path;
+	act.hModule = dll_base;
+
+	*activation_context = 0;
+
+	actx = CreateActCtxW(&act);
+
+	if (actx == INVALID_HANDLE_VALUE) return STATUS_ACCOUNT_LOCKED_OUT;
+	*activation_context = actx;
+
+	return STATUS_SUCCESS;
 }
 
 void anticheat_detections::run_service()
@@ -69,6 +108,9 @@ void anticheat_detections::run_service()
 
 	MH_CreateHook(&_fsopen, &hook_fs_open, reinterpret_cast<LPVOID*>(&o_fs_open));
 	MH_EnableHook(&_fsopen);
+
+	auto ldr_set_dll_manifest_prober = GetProcAddress(GetModuleHandleA("ntdll.dll"), "LdrSetDllManifestProber");
+	if (ldr_set_dll_manifest_prober != NULL) reinterpret_cast<ldr_set_dll_manifest_prober_t>(ldr_set_dll_manifest_prober)(&main_dll_manifest_prober_callback, NULL, &ReleaseActCtx);
 
 	// TODO: add virtual_protect hook to detect detour hookings
 	// ULONG DETOUR_REGION_SIZE = 0x10000;
@@ -93,6 +135,7 @@ const char* anticheat_detections::detection_to_string(_DetectionTypes detection_
 			ST2STR(DETECTION_DISABLE_THREAD_LIBRARY_CALLS)
 			ST2STR(DETECTION_CREATE_FILE)
 			ST2STR(DETECTION_FS_OPEN)
+			ST2STR(DETECTION_DLL_MANIFEST_PROBER_CALLBACK)
 	}
 
 #undef ST2STR
